4922d856491abdfa90fe2c3d3acb4af5f1463a84
AntLouiz <luizrodrigo46@hotmail.com>
2018-11-02 20:30:29 -0300

    organizing the main.py

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ from settings import (
     POPULATION_SIZE,
     SELECTION_PERCENT,
     MUTATION_PERCENT,
-    MAX_ITERATION
+    MAX_ITERATION,
+    ITENS
 )
 from chromosome import Chromosome
 from utils import (
     roulette_selection,
     crossover,
     chromosome_is_valid,
-    mutate
+    mutate,
+    best
 )
 
 population = []
 generation = 1
+
+print("Generating a valid population...")
 
 while len(population) != POPULATION_SIZE:
     gene = [random.randint(0, 1) for i in range(MAX_ITENS)]
@@ -25,33 +29,41 @@ while len(population) != POPULATION_SIZE:
     if chromosome_is_valid(c):
         population.append(c)
 
+print("\nInitial population Generated.")
+print("Searching the solution...")
 
-selected_population = []
-new_population = []
+while generation <= MAX_ITERATION:
+    selected_population = []
+    new_population = []
 
-random_selecteds_size = POPULATION_SIZE - int((POPULATION_SIZE * SELECTION_PERCENT) / 100)
+    random_selecteds_size = POPULATION_SIZE - int((POPULATION_SIZE * SELECTION_PERCENT) / 100)
 
-for i in range(random_selecteds_size):
-    selected_population.append(population[random.randint(0, len(population) - 1)])
-
-
-for i in range(int((POPULATION_SIZE * SELECTION_PERCENT) / 100)):
-    selected_population.append(roulette_selection(population))
-
-selected_population = zip(
-    selected_population,
-    selected_population[int(len(selected_population) / 2):]
-)
+    for i in range(random_selecteds_size):
+        selected_population.append(population[random.randint(0, len(population) - 1)])
 
 
-for x, y in selected_population:
-    new_population.append(crossover(x.gene, y.gene))
-    new_population.append(crossover(y.gene, x.gene))
+    for i in range(int((POPULATION_SIZE * SELECTION_PERCENT) / 100)):
+        selected_population.append(roulette_selection(population))
+
+    selected_population = zip(
+        selected_population,
+        selected_population[int(len(selected_population) / 2):]
+    )
 
 
-population_to_mutate = int((POPULATION_SIZE * MUTATION_PERCENT) / 100)
+    for x, y in selected_population:
+        new_population.append(crossover(x.gene, y.gene))
+        new_population.append(crossover(y.gene, x.gene))
 
-for i in range(population_to_mutate):
-    new_population[random.randint(0, MAX_ITENS - 1)] = mutate(new_population[random.randint(0, MAX_ITENS - 1)].gene)
 
-print(new_population)
+    population_to_mutate = int((POPULATION_SIZE * MUTATION_PERCENT) / 100)
+
+    for i in range(population_to_mutate):
+        new_population[random.randint(0, MAX_ITENS - 1)] = mutate(new_population[random.randint(0, MAX_ITENS - 1)].gene)
+
+    population = new_population
+
+    generation += 1
+
+print(ITENS)
+print(best(population).gene)
