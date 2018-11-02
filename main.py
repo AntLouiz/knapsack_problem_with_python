c8963746244005f4f01874918bca831b07cb5826
AntLouiz <luizrodrigo46@hotmail.com>
2018-11-02 00:30:42 -0300

    selecting random chromosomes to fill the population

--- main.py
+++ main.py
@@ -25,12 +25,15 @@ print('Population collected.')
 
 
 selected_population = []
+new_population = []
+random_selecteds_size = POPULATION_SIZE - int((POPULATION_SIZE * SELECTION_PERCENT) / 100)
+
+for i in range(random_selecteds_size):
+    selected_population.append(population[random.randint(0, len(population) - 1)])
+
 
 for i in range(int((POPULATION_SIZE * SELECTION_PERCENT) / 100)):
     selected_population.append(roulette_selection(population))
-
-print(len(population))
-print(len(selected_population))
 
 selected_population = zip(
     selected_population,
@@ -39,4 +42,7 @@ selected_population = zip(
 
 
 for x, y in selected_population:
-    chromosome_son = crossover(x, y)
+    new_population.append(crossover(x.gene, y.gene))
+    new_population.append(crossover(y.gene, x.gene))
+
+print(new_population)
